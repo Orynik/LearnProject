292b0748fd2cb48dacbd20dc732a46b81366b05f
Администратор <@WIN>
2020-01-28 12:38:29 +0800

    structured dirs

---
+++ College/С#/q/q/Sum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q
+{
+    class Sum
+    {
+        public int Summ(int a, int b)
+        {
+            int f = a + b;
+            return f;
+        }
+    }
+}
