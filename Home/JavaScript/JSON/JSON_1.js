642681d1ce9fd66d9c98cb24fa9587124745accd
Администратор <@WIN>
2020-02-05 11:47:37 +0800

    modificated .gitignore

--- Home/JavaScript/JSON/JSON_1.js
+++
@@ -1,8 +0,0 @@
-//@ts-check
-"use strict";
-let user = {
-    name: "Василий Иванович",
-    age: 35
-};
-let user_date = JSON.parse(JSON.stringify(user));
-document.write(`Result first quest: ${user_date}`)
