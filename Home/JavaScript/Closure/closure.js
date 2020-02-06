db7bfe794e6c758e628617e6fefc6928c1c5ab66
orynik <dkrovel@gmail.com>
2020-02-06 11:55:03 +0800

    Solution closure task

--- Home/JavaScript/Closure/closure.js
+++ Home/JavaScript/Closure/closure.js
@@ -1,3 +1,4 @@
+//1
 function sum(a){
 
   return function(b){
@@ -5,5 +6,18 @@ function sum(a){
   }
 }
 
-alert( sum(1)(2) );
-alert( sum(5)(-1) );
+// alert( sum(1)(2) );
+// alert( sum(5)(-1) );
+//2
+  let arr = [1, 2, 3, 4, 5, 6, 7];
+  let i = 0;
+  function inBetween(a, b) {
+    return (value) => value >=a && value <=b ? true: false;
+  };
+  
+  function inArray(arr) {
+    // ...your code...
+  }
+  // console.log(arr);
+  // debugger
+  console.log( arr.filter(inBetween(3,6)))
