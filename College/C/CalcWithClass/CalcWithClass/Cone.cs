042016ce7900eda5fa319df0182f0cb4f71f23cd
Orynik <dkrovel@gmail.com>
2020-02-10 20:21:18 +0800

    solution calc with clases

---
+++ College/C/CalcWithClass/CalcWithClass/Cone.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcWithClass
+{
+    public class Cone
+    {
+        int r;
+        int h;
+
+        public Cone(int r, int h)
+        {
+            this.r = r;
+            this.h = h;
+        }
+        public double CalcS()
+        {
+            double l = 2 * Math.PI * this.r;
+            return Math.Round(Math.PI * Math.Pow(this.r,2) + Math.PI * this.r * Math.Sqrt(Math.Pow(this.r,2) + Math.Pow(this.h,2)),2);
+        }
+
+        public double CalcV()
+        {
+            return Math.Round(0.3333 * Math.PI * Math.Pow(this.r, 2) * this.h,2);
+        }
+    }
+}
---
+++ College/C/CalcWithClass/CalcWithClass/Cylinder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcWithClass
+{
+    public class Cylinder
+    {
+        int r;
+        int h;
+        public Cylinder(int r, int h)
+        {
+            this.r = r;
+            this.h = h;
+        }
+        public double CalcS()
+        {
+            return Math.Round(2 * Math.PI * this.r * (this.r + this.h),2);
+        }
+        public double CalcV()
+        {
+            return Math.Round(Math.PI * Math.Pow(this.r,2) * this.h, 2);
+        }
+    }
+}
