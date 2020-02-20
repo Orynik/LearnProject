4fb52bb3e8c884d594976160b1ad81d370e21684
Администратор <@WIN>
2020-02-20 15:06:22 +0800

    Calc

---
+++ College/C/Calc/Calc/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace Calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TB = new System.Windows.Forms.TextBox();
+            this.Seven = new System.Windows.Forms.Button();
+            this.Five = new System.Windows.Forms.Button();
+            this.Six = new System.Windows.Forms.Button();
+            this.Eight = new System.Windows.Forms.Button();
+            this.Four = new System.Windows.Forms.Button();
+            this.One = new System.Windows.Forms.Button();
+            this.Two = new System.Windows.Forms.Button();
+            this.Three = new System.Windows.Forms.Button();
+            this.Zero = new System.Windows.Forms.Button();
+            this.Nine = new System.Windows.Forms.Button();
+            this.Sub = new System.Windows.Forms.Button();
+            this.Mul = new System.Windows.Forms.Button();
+            this.Add = new System.Windows.Forms.Button();
+            this.Div = new System.Windows.Forms.Button();
+            this.Equal = new System.Windows.Forms.Button();
+            this.C = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TB
+            // 
+            this.TB.Location = new System.Drawing.Point(12, 25);
+            this.TB.Name = "TB";
+            this.TB.ReadOnly = true;
+            this.TB.Size = new System.Drawing.Size(211, 20);
+            this.TB.TabIndex = 0;
+            // 
+            // Seven
+            // 
+            this.Seven.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Seven.Location = new System.Drawing.Point(12, 51);
+            this.Seven.Name = "Seven";
+            this.Seven.Size = new System.Drawing.Size(31, 23);
+            this.Seven.TabIndex = 1;
+            this.Seven.Text = "7";
+            this.Seven.UseVisualStyleBackColor = true;
+            this.Seven.Click += new System.EventHandler(this.Seven_Click);
+            // 
+            // Five
+            // 
+            this.Five.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Five.Location = new System.Drawing.Point(49, 80);
+            this.Five.Name = "Five";
+            this.Five.Size = new System.Drawing.Size(31, 23);
+            this.Five.TabIndex = 2;
+            this.Five.Text = "5";
+            this.Five.UseVisualStyleBackColor = true;
+            this.Five.Click += new System.EventHandler(this.Five_Click);
+            // 
+            // Six
+            // 
+            this.Six.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Six.Location = new System.Drawing.Point(86, 80);
+            this.Six.Name = "Six";
+            this.Six.Size = new System.Drawing.Size(31, 23);
+            this.Six.TabIndex = 3;
+            this.Six.Text = "6";
+            this.Six.UseVisualStyleBackColor = true;
+            this.Six.Click += new System.EventHandler(this.Six_Click);
+            // 
+            // Eight
+            // 
+            this.Eight.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Eight.Location = new System.Drawing.Point(49, 51);
+            this.Eight.Name = "Eight";
+            this.Eight.Size = new System.Drawing.Size(31, 23);
+            this.Eight.TabIndex = 4;
+            this.Eight.Text = "8";
+            this.Eight.UseVisualStyleBackColor = true;
+            this.Eight.Click += new System.EventHandler(this.Eight_Click);
+            // 
+            // Four
+            // 
+            this.Four.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Four.Location = new System.Drawing.Point(12, 80);
+            this.Four.Name = "Four";
+            this.Four.Size = new System.Drawing.Size(31, 23);
+            this.Four.TabIndex = 5;
+            this.Four.Text = "4";
+            this.Four.UseVisualStyleBackColor = true;
+            this.Four.Click += new System.EventHandler(this.Four_Click);
+            // 
+            // One
+            // 
+            this.One.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.One.Location = new System.Drawing.Point(12, 111);
+            this.One.Name = "One";
+            this.One.Size = new System.Drawing.Size(31, 23);
+            this.One.TabIndex = 6;
+            this.One.Text = "1";
+            this.One.UseVisualStyleBackColor = true;
+            this.One.Click += new System.EventHandler(this.One_Click);
+            // 
+            // Two
+            // 
+            this.Two.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Two.Location = new System.Drawing.Point(49, 111);
+            this.Two.Name = "Two";
+            this.Two.Size = new System.Drawing.Size(31, 23);
+            this.Two.TabIndex = 7;
+            this.Two.Text = "2";
+            this.Two.UseVisualStyleBackColor = true;
+            this.Two.Click += new System.EventHandler(this.Two_Click);
+            // 
+            // Three
+            // 
+            this.Three.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Three.Location = new System.Drawing.Point(86, 111);
+            this.Three.Name = "Three";
+            this.Three.Size = new System.Drawing.Size(31, 23);
+            this.Three.TabIndex = 8;
+            this.Three.Text = "3";
+            this.Three.UseVisualStyleBackColor = true;
+            this.Three.Click += new System.EventHandler(this.Three_Click);
+            // 
+            // Zero
+            // 
+            this.Zero.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Zero.Location = new System.Drawing.Point(12, 140);
+            this.Zero.Name = "Zero";
+            this.Zero.Size = new System.Drawing.Size(105, 23);
+            this.Zero.TabIndex = 9;
+            this.Zero.Text = "0";
+            this.Zero.UseVisualStyleBackColor = true;
+            this.Zero.Click += new System.EventHandler(this.Zero_Click);
+            // 
+            // Nine
+            // 
+            this.Nine.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Nine.Location = new System.Drawing.Point(86, 51);
+            this.Nine.Name = "Nine";
+            this.Nine.Size = new System.Drawing.Size(31, 23);
+            this.Nine.TabIndex = 10;
+            this.Nine.Text = "9";
+            this.Nine.UseVisualStyleBackColor = true;
+            this.Nine.Click += new System.EventHandler(this.Nine_Click);
+            // 
+            // Sub
+            // 
+            this.Sub.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Sub.Location = new System.Drawing.Point(121, 82);
+            this.Sub.Name = "Sub";
+            this.Sub.Size = new System.Drawing.Size(29, 23);
+            this.Sub.TabIndex = 11;
+            this.Sub.Text = "-";
+            this.Sub.UseVisualStyleBackColor = true;
+            this.Sub.Click += new System.EventHandler(this.Sub_Click);
+            // 
+            // Mul
+            // 
+            this.Mul.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Mul.Location = new System.Drawing.Point(121, 51);
+            this.Mul.Name = "Mul";
+            this.Mul.Size = new System.Drawing.Size(29, 23);
+            this.Mul.TabIndex = 12;
+            this.Mul.Text = "*";
+            this.Mul.UseVisualStyleBackColor = true;
+            this.Mul.Click += new System.EventHandler(this.Mul_Click);
+            // 
+            // Add
+            // 
+            this.Add.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Add.Location = new System.Drawing.Point(121, 111);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(29, 23);
+            this.Add.TabIndex = 13;
+            this.Add.Text = "+";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Div
+            // 
+            this.Div.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Div.Location = new System.Drawing.Point(123, 140);
+            this.Div.Name = "Div";
+            this.Div.Size = new System.Drawing.Size(27, 23);
+            this.Div.TabIndex = 14;
+            this.Div.Text = "/";
+            this.Div.UseVisualStyleBackColor = true;
+            this.Div.Click += new System.EventHandler(this.Div_Click);
+            // 
+            // Equal
+            // 
+            this.Equal.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Equal.Location = new System.Drawing.Point(156, 96);
+            this.Equal.Name = "Equal";
+            this.Equal.Size = new System.Drawing.Size(75, 67);
+            this.Equal.TabIndex = 15;
+            this.Equal.Text = "=";
+            this.Equal.UseVisualStyleBackColor = true;
+            this.Equal.Click += new System.EventHandler(this.Equal_Click);
+            // 
+            // C
+            // 
+            this.C.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.C.Location = new System.Drawing.Point(156, 51);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(75, 39);
+            this.C.TabIndex = 16;
+            this.C.Text = "z";
+            this.C.UseVisualStyleBackColor = true;
+            this.C.Click += new System.EventHandler(this.C_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(235, 175);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.Equal);
+            this.Controls.Add(this.Div);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.Mul);
+            this.Controls.Add(this.Sub);
+            this.Controls.Add(this.Nine);
+            this.Controls.Add(this.Zero);
+            this.Controls.Add(this.Three);
+            this.Controls.Add(this.Two);
+            this.Controls.Add(this.One);
+            this.Controls.Add(this.Four);
+            this.Controls.Add(this.Eight);
+            this.Controls.Add(this.Six);
+            this.Controls.Add(this.Five);
+            this.Controls.Add(this.Seven);
+            this.Controls.Add(this.TB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TB;
+        private System.Windows.Forms.Button Seven;
+        private System.Windows.Forms.Button Five;
+        private System.Windows.Forms.Button Six;
+        private System.Windows.Forms.Button Eight;
+        private System.Windows.Forms.Button Four;
+        private System.Windows.Forms.Button One;
+        private System.Windows.Forms.Button Two;
+        private System.Windows.Forms.Button Three;
+        private System.Windows.Forms.Button Zero;
+        private System.Windows.Forms.Button Nine;
+        private System.Windows.Forms.Button Sub;
+        private System.Windows.Forms.Button Mul;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Div;
+        private System.Windows.Forms.Button Equal;
+        private System.Windows.Forms.Button C;
+    }
+}
+
---
+++ College/C/Calc/Calc/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calc
+{
+    public partial class Form1 : Form
+    {
+        string text = "";
+        char znak = ' ';
+        int secondIdx = 0;
+        int i = 0;
+        bool end = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void One_Click(object sender, EventArgs e)
+        {
+            clear();
+            writeInTB("1");
+        }
+
+        private void Two_Click(object sender, EventArgs e)
+        {
+            clear();
+            writeInTB("2");
+        }
+
+        private void Three_Click(object sender, EventArgs e)
+        {
+            clear();
+            writeInTB("3");
+        }
+
+        private void Four_Click(object sender, EventArgs e)
+        {
+            clear();
+            writeInTB("4");
+        }
+
+        private void Five_Click(object sender, EventArgs e)
+        {
+            clear();
+            writeInTB("5");
+        }
+
+        private void Six_Click(object sender, EventArgs e)
+        {
+            clear();
+            writeInTB("6");
+        }
+
+        private void Seven_Click(object sender, EventArgs e)
+        {
+            clear();
+            writeInTB("7");
+        }
+
+        private void Eight_Click(object sender, EventArgs e)
+        {
+            clear();
+            writeInTB("8");
+        }
+
+        private void Nine_Click(object sender, EventArgs e)
+        {
+            clear();
+            writeInTB("9");
+        }
+
+        private void Zero_Click(object sender, EventArgs e)
+        {
+            clear();
+            writeInTB("0");
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            prepareForDo('+');
+        }
+
+        private void Equal_Click(object sender, EventArgs e)
+        {
+            string[] arr = TB.Text.Split(znak);
+            string q = TB.Text.Replace(znak, ' ');
+            string[] text = q.Split(' ');
+            if (TB.Text.IndexOf(znak) != -1 && znak == '+')
+            {
+                int w = 0;
+                for (int i =0; i< text.Length; i++) {
+                    w += Convert.ToInt32(text[i]);
+                }
+                TB.Text = Convert.ToString(w);
+            }
+            else if (TB.Text.IndexOf(znak) != -1 && znak == '-')
+            {
+                int w = Convert.ToInt32(text[0]);
+                for (int i = 1; i < text.Length; i++)
+                {
+                    w -= Convert.ToInt32(text[i]);
+                }
+                TB.Text = Convert.ToString(w);
+            }
+            else if (TB.Text.IndexOf(znak) != -1 && znak == '*')
+            {
+                int w = Convert.ToInt32(text[0]);
+                for (int i = 1; i < text.Length; i++)
+                {
+                    w *= Convert.ToInt32(text[i]);
+                }
+                TB.Text = Convert.ToString(w);
+            }
+            else if (TB.Text.IndexOf(znak) != -1 && znak == '/')
+            {
+                int w = Convert.ToInt32(text[0]);
+                for (int i = 1; i < text.Length; i++)
+                {
+                    w /= Convert.ToInt32(text[i]);
+                }
+                TB.Text = Convert.ToString(w);
+            }
+        }
+
+        private void writeInTB(string value)
+        {
+            text += value;
+            TB.Text += value;
+        }
+        
+        private void prepareForDo(char zk)
+        {
+            znak = zk;
+            TB.Text += znak;
+        }
+
+        private void Sub_Click(object sender, EventArgs e)
+        {
+            prepareForDo('-');
+        }
+
+        private void Mul_Click(object sender, EventArgs e)
+        {
+            prepareForDo('*');
+        }
+
+        private void Div_Click(object sender, EventArgs e)
+        {
+            prepareForDo('/');
+        }
+        private void clear()
+        {
+            if (end)
+            {
+                TB.Text = "";
+            }
+            end = false;
+        }
+
+        private void C_Click(object sender, EventArgs e)
+        {
+            TB.Text = "";
+        }
+    }
+}
