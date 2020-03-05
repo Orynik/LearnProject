8acdcce88271b48611a9a108955b76157ae71a30
Администратор <@WIN>
2020-03-05 10:19:31 +0800

    Проектирование и создание тз Калькулятора

---
+++ College/PHP/Calc_SpiralMethod/Калькулятор.php
@@ -0,0 +1,89 @@
+<?php
+namespace Package1;
+
+/**
+ * 
+ */
+class Калькулятор {
+
+	/**
+	 * 
+	 */
+	public $a;
+
+	/**
+	 * 
+	 */
+	public $b;
+
+	/**
+	 * 
+	 */
+	public $result;
+
+	/**
+	 * Default constructor
+	 */
+	public function __construct() {
+	}
+
+	/**
+	 * @param $a 
+	 * @param $b 
+	 * @return
+	 */
+	public function DefiningAction($a, $b) {
+		// TODO implement here
+		return 0;
+	}
+
+	/**
+	 * @param $a 
+	 * @param $b 
+	 * @return
+	 */
+	public function Sum($a, $b) {
+		// TODO implement here
+		return 0;
+	}
+
+	/**
+	 * @param $a 
+	 * @param $b 
+	 * @return
+	 */
+	public function Difference($a, $b) {
+		// TODO implement here
+		return 0;
+	}
+
+	/**
+	 * @param $a 
+	 * @param $b 
+	 * @return
+	 */
+	public function Multiplication($a, $b) {
+		// TODO implement here
+		return 0;
+	}
+
+	/**
+	 * @param $a 
+	 * @param $b 
+	 * @return
+	 */
+	public function Division($a, $b) {
+		// TODO implement here
+		return 0;
+	}
+
+	/**
+	 * @param $a 
+	 * @return
+	 */
+	public function Pow($a) {
+		// TODO implement here
+		return 0;
+	}
+ 
+}
